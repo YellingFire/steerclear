551ccfb5b9a5a7c21b18b6c89fc34cee94cb8e23
Chad33 <chad.javarush@gmail.com>
2018-07-24 19:18:32 -0600

    changed staging environment

--- client/src/environment/determineEnv.js
+++ client/src/environment/determineEnv.js
@@ -1,8 +1,8 @@
 export default function determineEnv() {
     switch(window.location.origin) {
         case 'http://localhost:3000': return 'DEV';
-        case '<YOUR STAGING ENVIRONMENT HERE>': return 'STAGING';
-        case '<YOUR PRODUCTION ENVIRONMENT HERE>': return 'PROD';
+        // case '<YOUR STAGING ENVIRONMENT HERE>': return 'STAGING';
+        case 'https://fast-shelf-49291.herokuapp.com': return 'PROD';
         default: throw new Error('Your current environment is not configured in determineEnv.js');
     }
 }
